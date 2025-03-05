5fa9ab92d65d46e9605027eac26ad2413f83f4cc
antony-souza <antonygustavo09@hotmail.com>
2025-03-05 13:17:04 -0300

    Reduzindo o uso de tab.navgator

--- app/main/main.tsx
+++ app/main/main.tsx
@@ -1,32 +1,44 @@
-﻿import { StatusBarThemed, Text, View } from "@/components/Themed";
-import MapView from "react-native-maps";
+﻿import Work from "@/app/main/tabs/work/work";
+import Cloud from "@/app/cloud/cloud";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { View } from "@/components/Themed";
 import { styles } from "@/app/main/styles";
-import WorkToggle from "@/app/main/components/work-toggle";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Profile } from "./tabs/profile/profile";
 
 const Tab = createBottomTabNavigator();
-import Ionicons from "@expo/vector-icons/Ionicons";
-import Work from "@/app/main/tabs/work/work";
-import Cloud from "@/app/cloud/cloud";
+
+interface ITab {
+	name: string;
+	icon: keyof typeof Ionicons.glyphMap;
+	component: () => JSX.Element;
+}
+
+const tabs: ITab[] = [
+	{ name: "Home", icon: "home", component: Work, },
+	/* { name: "Profile", icon: "person", component: Profile }, */
+	{ name: "Download", icon: "cloud-download", component: () => <Cloud manualDownload={true} /> }
+];
 
 export default function Main() {
 	return (
 		<View style={styles.container}>
 			<Tab.Navigator
-				screenOptions={({ route }) => ({
-					tabBarIcon: ({ color, size }) => {
-						return <Ionicons name="home" size={size} color={color} />;
-					},
+				screenOptions={() => ({
 					tabBarActiveTintColor: "blue",
 					tabBarInactiveTintColor: "gray",
-					headerShown: false
+					headerShown: false,
 				})}
 			>
-				<Tab.Screen name="Home" component={Work} />
-				<Tab.Screen name="Download" component={() => {
-					return <Cloud manualDownload={true} />;
-				}} />
+				{tabs.map((tab) => (
+					<Tab.Screen key={tab.name} name={tab.name} component={tab.component}
+						options={{
+							tabBarIcon: ({ color, size }) => (
+								<Ionicons name={tab.icon} size={size} color={color} />
+							)
+						}}
+					/>
+				))}
 			</Tab.Navigator>
 		</View>
 	);
